ad0bb2d4a225213c1347899ef1fe2296ffeef563
lee <276007849@qq.com>
2018-06-06 21:35:19 +0800

    2018/6/6晚上修改

--- src/main/java/com/smart/domain/Post.java
+++ src/main/java/com/smart/domain/Post.java
@@ -1,5 +1,6 @@
 package com.smart.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
@@ -10,10 +11,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import com.sun.jna.platform.win32.Netapi32Util.User;
 
 
 @Entity
@@ -27,7 +32,61 @@ public class Post {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="post_id")
 	private int postId;
-	
+	@Column(name="post_title")
+	private String postTitle;
+	@Column(name="post_text")
+	private String postText;
+	@Column(name="board_id")
+	private String boardId;
+	@Column(name="create_time")
+	private String createTime;
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	@ManyToOne(cascade= {CascadeType.PERSIST,CascadeType.MERGE})
+	private Topic topic;
+	public int getPostId() {
+		return postId;
+	}
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+	public String getPostTitle() {
+		return postTitle;
+	}
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+	public String getPostText() {
+		return postText;
+	}
+	public void setPostText(String postText) {
+		this.postText = postText;
+	}
+	public String getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(String boardId) {
+		this.boardId = boardId;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Topic getTopic() {
+		return topic;
+	}
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
 	
 	
 	
